21c1f5feab3ba42f9b101389cc626307d336855d
xqwerty11 <hk637999@gmail.com>
2026-08-27 20:57:59 +0900

    클래스의 접근 지정자

---
+++ 5주차/목요일/src/목요일2026_08_27/클래스의_접근_지정자.java
@@ -0,0 +1,15 @@
+package 목요일2026_08_27;
+
+public class 클래스의_접근_지정자 {
+
+	public static void main(String[] args) {
+		/**
+		 * 이제 클래스의 접근 지정자를 알아보자 클래스에서는 public,default 접근 지정자만 사용할 수 있다. 
+		 * 쉽게 말해 class 키워드 앞에 public이 붙어 있거나 붙어 있지 않거나(default)이다 
+		 * 그렇다면 클래스가 public이라는 것과 default라는 것은 어떤 의미가 있을까?
+		 * 앞에서와 마찬가지로 default 클래스는 같은 패키지 내에서만 사용할 수 있고 public 클래스는 다른 패키지에서도 사용할 수 있다.
+		 * 클래스를 default로 정의하면 다른 패키지에서 임포트가 불가능해 사용할 수 없게 되는 것이다.
+		 */
+	}
+
+}
